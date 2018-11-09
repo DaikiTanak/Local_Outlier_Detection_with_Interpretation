b0dea41a14b946d3ae906a14ef5e1418e403f88d
Daiki Tanaka <daiki.yosky@gmail.com>
2018-11-09 14:45:08 +0900

    Add files via upload

--- lodi.py
+++ lodi.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 from sklearn.neighbors import NearestNeighbors
+from sklearn.utils.extmath import randomized_svd
+
 import math
 import copy
 from tqdm import tqdm_notebook as tqdm
 import pyper
 from statistics import mean, median,variance,stdev
 from scipy import linalg
+
+import pandas as pd
+
+import time
 
 class LODI(object):
     """
@@ -118,7 +124,7 @@ class LODI(object):
         return np.array(true_neighbors_list)
 
 
-    def anomalous_degree(self):
+    def anomalous_degree(self, svd="normal"):
         """
             Compute Local anomalous degree of every data.
             When getting optimal w, I use [Tan+ 2018] method.
@@ -133,7 +139,11 @@ class LODI(object):
         w_list = []
         lambda_list = []
         AD_list = []
+
+
+        avg_time = 0
         for target, neighbors in zip(tqdm(X), neighbors_list):
+
             #print(np.matrix(neighbors).shape)
             e = np.matrix(np.ones(len(neighbors))).T
             R = np.matrix(neighbors).T
@@ -142,66 +152,101 @@ class LODI(object):
 
             # J(w)AA.T = BB.Tw
             # These metrices are d*d.
-            AA = A.dot(A.T)
-            BB = B.dot(B.T)
+            AA_t = A.dot(A.T)
+            BB_t = B.dot(B.T)
+
+
+            
+            if svd == "normal":
+                U, s, V = linalg.svd(A)
+
+                singular_values_sum = np.sum(s)
+                rank_threshold = 0.95 * singular_values_sum
+
+                memo = 0
+                rank = 0
+                for singular_value in s:
+                     memo += singular_value
+                     rank += 1
+                     if memo > rank_threshold:
+                         break
+
+                Ur = U[:, :rank]
+                sr = np.matrix(linalg.diagsvd(s[:rank], rank,rank))
+                Vr = V[:rank, :]
+                # Aの近似
+                A_ = np.asarray(Ur*sr*Vr)
+                #print(A, A_)
+
+                sr_ = sr**(-2)
+                new_matrix = Ur*sr_*Ur.T*B*B.T
+                lambda_, vr = linalg.eig(new_matrix, right=True, left=False)
+                #print(lambda_)
+
+                w = vr[:,np.argmax(lambda_)]
+
+            
+            now = time.time()
+            # Randomized SVD
+            if svd == "random":
+
+                U, s, V = randomized_svd(A, 10, random_state=46)
+
+                singular_values_sum = np.sum(s)
+                rank_threshold = 1.0 * singular_values_sum
+
+                memo = 0
+                rank = 0
+                for singular_value in s:
+                     memo += singular_value
+                     rank += 1
+                     if memo > rank_threshold:
+                         break
+
+                Ur = U[:, :rank]
+                sr = np.matrix(linalg.diagsvd(s[:rank], rank,rank))
+                Vr = V[:rank, :]
+                # Aの近似
+                A_ = np.asarray(Ur*sr*Vr)
+
+                sr_ = sr**(-2)
+                new_matrix = Ur*sr_*Ur.T*B*B.T
+                lambda_, vr = linalg.eig(new_matrix, right=True, left=False)
+                w = vr[:,np.argmax(lambda_)]
 
 
 
-            U, s, V = linalg.svd(A)
-            singular_values_sum = np.sum(s)
-            rank_threshold = 0.95 * singular_values_sum
+            
+            if svd == "sparse":
+                r = pyper.R()
 
-            memo = 0
-            rank = 0
-            for singular_value in s:
-                 memo += singular_value
-                 rank += 1
-                 if memo > rank_threshold:
-                     break
+                n=50
+                p=25
 
-            Ur = U[:, :rank]
-            sr = np.matrix(linalg.diagsvd(s[:rank], rank,rank))
-            Vr = V[:rank, :]
-            A_ = np.asarray(Ur*sr*Vr)
+                # Using https://arxiv.org/abs/1604.08697 to get optimal w.
+                # The R implementation is here: https://cran.r-project.org/web/packages/rifle/index.html
+                # set parameters.
+                r("source(file='function.R')")
+                r("source(file='initial.convex.R')")
+                r("source(file='rifle.R')")
+                r.assign("A", BB_t)
+                r.assign("B", AA_t)
+                r.assign("K", 1)
+                r.assign("lambda", 2*np.sqrt(np.log(p)/n))
+                r("v = initial.convex(A,B,lambda,K,nu=1,trace=FALSE)")
+                r("init = eigen(v$Pi+t(v$Pi))$vectors[,1]")
+                r("k <- 10")
+                r("xprime <- rifle(A,B,init,k,0.01,1e-3)")
 
-            sr_ = sr**(-2)
-            new_matrix = Ur*sr_*Ur.T*B*B.T
-            print(new_matrix.shape)
-            lambda_, vr = linalg.eig(new_matrix, right=True, left=False)
-            w = vr[:,0]
+                w = np.matrix(r.get("xprime"))
+                wAAw = w.T.dot(AA_t).dot(w)
+                wBBw = w.T.dot(BB_t).dot(w)
+                max_lambda = np.array(wBBw/wAAw)[0][0]
+                lambda_list.append(max_lambda)
+            
+            avg_time += time.time()-now
 
-
-
-            """
-            r = pyper.R()
-
-            n=50
-            p=25
-
-            # Using https://arxiv.org/abs/1604.08697 to get optimal w.
-            # The R implementation is here: https://cran.r-project.org/web/packages/rifle/index.html
-            # set parameters.
-            r("source(file='function.R')")
-            r("source(file='initial.convex.R')")
-            r("source(file='rifle.R')")
-            r.assign("A", BB)
-            r.assign("B", AA)
-            r.assign("K", 1)
-            r.assign("lambda", 2*np.sqrt(np.log(p)/n))
-            r("v = initial.convex(A,B,lambda,K,nu=1,trace=FALSE)")
-            r("init = eigen(v$Pi+t(v$Pi))$vectors[,1]")
-            r("k <- 10")
-            r("xprime <- rifle(A,B,init,k,0.01,1e-3)")
-
-            w = np.matrix(r.get("xprime"))
-            wAAw = w.T.dot(AA).dot(w)
-            wBBw = w.T.dot(BB).dot(w)
             w_list.append(w)
-            max_lambda = np.array(wBBw/wAAw)[0][0]
-            lambda_list.append(max_lambda)
-            """
-
-
             # Compute Anomlous Degree.
             # Variance of neighbors on the projection.
             projected_neighbors = np.matrix(neighbors).dot(w)
@@ -213,10 +258,11 @@ class LODI(object):
             AD = max(AD, np.sqrt(var_neighbors))
             AD_list.append(AD)
 
+        print("avg svd time : {}".format(avg_time/len(X)))
         #Compute Local Anomolous Degree
         LAD_list = []
-        for target_index, neighbors_index in zip(tqdm(range(len(X))), self.neighbors_index_list):
-            print("target and its neighbors:",target_index, neighbors_index)
+        for target_index, neighbors_index in zip(range(len(X)), self.neighbors_index_list):
+            #print("target and its neighbors:",target_index, neighbors_index)
             target_AD = AD_list[target_index]
             neighbors_AD = []
             for nei_index in neighbors_index:
@@ -226,9 +272,16 @@ class LODI(object):
         self.LAD_list = LAD_list
         self.w_list = np.array(w_list)
 
-        return LAD_list
 
-    def interpret_outliers(self, lambda_=0.8):
+        #outlier_num=2
+
+        #outlier_index = np.argsort(LAD_list)[-1::-1][:outlier_num]
+        #for index in outlier_index:
+        #    print("outlier:", X[index])
+
+        return LAD_list, w_list
+
+    def interpret_outliers(self, lambda_=0.8, outlier_num=10):
         """
             Give interpretation to data points.
         """
@@ -257,18 +310,58 @@ class LODI(object):
             interpretation.append(effective_feature_index)
             importances_list.append(importances)
 
-        K = 1
-        outliers_index = np.argsort(LAD_list)[-1::-1][:K]
+        #outlier_num = 4
+        outliers_index = np.argsort(LAD_list)[-1::-1][:outlier_num]
+        print("top{} outliers".format(outlier_num))
         for index in outliers_index:
-            print("data:", self.X[index])
-            print("explanation:", interpretation[index])
-            print("importances:", importances_list[index])
+            pass
+            #print("data index:", index)
+            #print("data:", self.X[index])
+            #print("効いてる次元:", interpretation[index])
+            #print("importances:", importances_list[index])
+            #print("-----------------------------------------------------------")
 
-        return interpretation
+        return outliers_index, interpretation, importances_list
 
     def run(self, lam):
-        _ = search_neighbors()
-        LAD_list = anomalous_degree()
-        explanation = interpret_outliers(lambda_=lam)
+        _ = self.search_neighbors()
+        LAD_list, w_list = self.anomalous_degree()
+        explanation = self.interpret_outliers(lambda_=lam)
 
         return LAD_list, explanation
+
+
+if __name__ == "__main__":
+    import pickle
+
+    train = pd.read_csv("input.csv")
+    columns = train.columns
+
+    X = np.array(train.iloc[:,1:])
+    y = np.array(train["class"])
+
+    
+    normal, sick = [], []
+    for data,label in zip(X,y):
+        if label == -1:
+            normal.append(data)
+        else:
+            sick.append(data)
+            
+    X = np.vstack((normal, [sick[0]]))
+
+
+    lodi = LODI(X, min_k=10, max_k=20)
+    LAD_, exp_ = lodi.run(0.8)
+
+    with open('LAD.pickle', mode='wb') as f:
+        pickle.dump(LAD_, f)
+    with open('exp.pickle', mode='wb') as f:
+        pickle.dump(exp_, f)
+
+
+
+
+
+
+
